c9c661b87d6f81a218326df8795bd2eb378f9601
LucaBreebaart <luca.breebaart99@gmail.com>
2024-07-31 22:14:58 +0200

    dashboard page structure setup

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="body-main">
+        <div class="code-embed w-embed"><svg width="1440" height="601" viewbox="0 0 1440 601" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g filter="url(#filter0_f_65_11)">
+            <rect x="226" y="285" width="1146.7" height="1146.7" rx="573.35" fill="url(#paint0_radial_65_11)" fill-opacity="0.3"></rect>
+          </g>
+          <defs>
+            <filter id="filter0_f_65_11" x="-58.3058" y="0.694183" width="1715.31" height="1715.31" filterunits="userSpaceOnUse" color-interpolation-filters="sRGB">
+              <feflood flood-opacity="0" result="BackgroundImageFix">
+                <feblend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape">
+                  <fegaussianblur stddeviation="142.153" result="effect1_foregroundBlur_65_11">
+                  </fegaussianblur></feblend></feflood></filter>
+            <radialgradient id="paint0_radial_65_11" cx="0" cy="0" r="1" gradientunits="userSpaceOnUse" gradienttransform="translate(799.35 858.35) rotate(90) scale(573.35)">
+              <stop offset="0.204962" stop-color="#BDA5E5"></stop>
+              <stop offset="1" stop-color="#2F2D7C"></stop>
+            </radialgradient>
+          </defs>
+        </svg></div>
+        <div class="nav-header">
+          <div class="nav-header--heading">
+            <div class="nav-header--h1">Welcome Back, Luca</div>
+            <div class="h4">Buy, Transfer and Sell chips all in one place</div>
+          </div>
+          <div class="nav-header--controls">
+            <div class="svg"></div>
+            <div class="svg"></div>
+          </div>
+        </div>
+        <div class="body-container">
+          <div class="body-container--left">
+            <div class="profile-container">
+              <div class="profile-sub-container">
+                <div class="profile-name-container">
+                  <div class="profile-image-container"></div>
+                  <div class="profile-container-heading">
+                    <div class="h1">LUCA BREEBAART</div>
+                    <div class="h4">LUCABREEBRT131513511</div>
+                  </div>
+                </div>
+                <div class="profile-content-container">
+                  <div class="profile-content">
+                    <div class="content">
+                      <div class="h3">Current Level</div>
+                      <div class="svg"></div>
+                    </div>
+                    <div class="h3">Gold</div>
+                  </div>
+                  <div class="profile-content">
+                    <div class="content">
+                      <div class="h3">Current Level</div>
+                      <div class="svg"></div>
+                    </div>
+                    <div class="h3">Gold</div>
+                  </div>
+                  <div class="profile-content">
+                    <div class="content">
+                      <div class="h3">Current Level</div>
+                      <div class="svg"></div>
+                    </div>
+                    <div class="h3">Gold</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cta-container">
+              <div class="btn-main dark-bg--gradient">
+                <div class="svg"></div>
+                <div class="h2">BUY</div>
+              </div>
+              <div class="btn-main dark-bg--gradient"></div>
+              <div class="btn-main dark-bg--gradient"></div>
+            </div>
+            <div class="snippet-container">
+              <div class="transactions-container dark-bg--gradient">
+                <div class="transaction-heading">
+                  <div class="h2">Transactions</div>
+                  <div class="h4">view all</div>
+                </div>
+                <div class="transaction-heading">
+                  <div class="transaction-user-container">
+                    <div></div>
+                    <div class="transaction-user-text-container">
+                      <div class="h3">Luca Breebaart</div>
+                      <div class="h4">31 july, 8pm</div>
+                    </div>
+                  </div>
+                  <div class="h3 green">Star Coin</div>
+                </div>
+                <div class="line"></div>
+                <div class="line"></div>
+              </div>
+              <div class="transactions-container dark-bg--gradient">
+                <div class="transaction-heading">
+                  <div class="h2">Transactions</div>
+                  <div class="h4">view all</div>
+                </div>
+                <div class="transaction-heading">
+                  <div class="transaction-user-container">
+                    <div></div>
+                    <div class="transaction-user-text-container">
+                      <div class="h3">Luca Breebaart</div>
+                      <div class="h4">31 july, 8pm</div>
+                    </div>
+                  </div>
+                  <div class="h3 green">Star Coin</div>
+                </div>
+                <div class="line"></div>
+                <div class="line"></div>
+              </div>
+            </div>
+          </div>
+          <div class="body-container--right">
+            <div class="portfolio-heading-container">
+              <div class="h4">Total Portfolio</div>
+              <div class="line"></div>
+              <div class="h4">R2 105.36</div>
+            </div>
+            <div class="portfolio-card-container">
+              <div class="coin-card dark-bg--gradient">
+                <div class="h3">Star Coin</div>
+                <div class="coin-card--heading">15.25 <span class="coin-card--heading--small">STRP</span> </div>
+                <div class="h3 green">+2.3%</div>
+                <div class="blur-block"></div>
+              </div>
+              <div class="coin-card dark-bg--gradient">
+                <div class="h3">Star Coin</div>
+                <div class="coin-card--heading">15.25 <span class="coin-card--heading--small">STRP</span> </div>
+                <div class="h3 green">+2.3%</div>
+                <div class="blur-block"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="nav-bar">
+        <div class="svg"></div>
+        <div class="svg"></div>
+        <div class="svg"></div>
+        <div class="svg"></div>
+      </div>
     </div>
   );
 }
