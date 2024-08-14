b6668dbbbdb693ec2c82d0568c7f6d7cd9109250
LucaBreebaart <luca.breebaart99@gmail.com>
2024-08-14 23:14:01 +0200

    transaction page and table setup

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import AuthService from '../src/services/authService';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard/Dashboard';
+import Transactions from './Pages/Transactions/Transactions';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
                     element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
                 /> */}
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/Transactions" element={<Transactions />} />
                 {/* <Route path="*" element={<Navigate to="/login" />} /> */}
             </Routes>
         </Router>
