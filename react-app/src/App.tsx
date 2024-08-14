d8fe509c509563397e6026d24f4fa47c8cee1d72
LucaBreebaart <luca.breebaart99@gmail.com>
2024-08-14 19:37:32 +0200

    Mareli login and signup initial functionality

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthService from '../src/services/authService';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
+
+    // const isAuthenticated = localStorage.getItem('user'); // Check for authentication
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                {/* <Route
+                    path="/dashboard"
+                    element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+                /> */}
                 <Route path="/dashboard" element={<Dashboard />} />
+                {/* <Route path="*" element={<Navigate to="/login" />} /> */}
             </Routes>
         </Router>
     );
 }
+
+const handleLogout = () => {
+    AuthService.logout();
+
+};
+
 export default App;
