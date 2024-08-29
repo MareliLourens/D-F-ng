1d98dd06d6d6f9b95ba42d3e540c5bb00ef353a0
DanteCS25 <cs.dantestrijdom@gmail.com>
2024-08-29 15:38:14 +0200

    Admin Page
    With the view of all accounts and the functionality to freeze account

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Screens/Login';
@@ -16,4 +17,49 @@ function App() {
     );
 }
 
+=======
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthService from '../src/services/authService';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Transactions from './Pages/Transactions/Transactions';
+import SingleCoin from './Pages/SingleCoin/SingleCoin';
+import SignUp from './Pages/SignUp';
+import Log from './Pages/Log';
+import AdminControl from './Pages/AdminControl/Admin';
+import AdminViewAccount from './Pages/AdminControl/AdminViewAccount';
+
+function App() {
+
+    // const isAuthenticated = localStorage.getItem('user'); // Check for authentication
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Log />} />
+                <Route path="/signup" element={<SignUp />} />
+                {/* <Route
+                    path="/dashboard"
+                    element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+                /> */}
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/Transactions" element={<Transactions />} />
+                <Route path="/single" element={<SingleCoin />} />
+                <Route path="/admin" element={<AdminControl />} />
+                <Route path="/admin/view-account" element={<AdminViewAccount />} />
+                
+
+                {/* <Route path="*" element={<Navigate to="/login" />} /> */}
+            </Routes>
+        </Router>
+    );
+}
+
+// const handleLogout = () => {
+//     AuthService.logout();
+
+// };
+
+>>>>>>> Stashed changes
 export default App;
---
+++ react-app/src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,240 @@
+import React, { useState, useEffect } from 'react';
+import './Dashboard.css';
+import { IoGridOutline } from "react-icons/io5";
+import { FaFireFlameCurved, FaMoneyBill, FaUserCheck, FaCoins, FaArrowRight, FaMoneyBillTransfer, FaMoneyBills } from "react-icons/fa6"
+import { SlGraph } from "react-icons/sl";
+import LineChart from '../../components/LineChart';
+import { useLocation } from 'react-router-dom';
+
+// components
+import NavBar from '../../components/navBar/NavBar';
+import NavHeader from '../../components/navHeader/navHeader';
+import Buy from '../../components/buy/Buy';
+import Sell from '../../components/sell/Sell';
+
+import useUserService, { UserData, AccountData } from '../../services/UserService';
+
+function Dashboard() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [accountData, setAccountData] = useState<AccountData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const userService = useUserService();
+  const location = useLocation();
+  const selectedUser = location.state?.selectedUser;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const user = await userService.getCurrentUser();
+        setUserData(user);
+
+        const account = await userService.getAccountData(user.userId);
+        setAccountData(account);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="body-main">
+        <NavHeader></NavHeader>
+        <div className="body-container">
+          <div className="body-container--left">
+            <div className="profile-container">
+              <div className="profile-sub-container">
+                <div className="profile-name-container">
+                  <div className="profile-image-container">
+                    <img src="https://images.unsplash.com/photo-1707759642885-42994e023046?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" className="profile-image" />
+                  </div>
+                  <div className="profile-container-heading">
+                    <div className="h1">{selectedUser ? selectedUser.username : userData?.username}</div>
+                    <div className="h4">{selectedUser ? selectedUser.email : userData?.email}</div>
+                  </div>
+                </div>
+                <div className="profile-content-container">
+                  <div className="profile-content">
+                    <div className="content">
+                      <div className="h3">Current Level</div>
+                      <div>
+                        <FaFireFlameCurved className="svg" />
+                      </div>
+                    </div>
+                    <div className="h3">Bronze</div>
+                  </div>
+                  <div className="profile-content">
+                    <div className="content">
+                      <div className="h3">Active</div>
+                      <div>
+                        <FaUserCheck className="svg" />
+                      </div>
+                    </div>
+                    <div className="h3">{accountData?.active ? 'True' : 'False'}</div>
+                  </div>
+                  <div className="profile-content">
+                    <div className="content">
+                      <div className="h3">Intrest Rate</div>
+                      <div>
+                        <SlGraph className="svg" />
+                      </div>
+                    </div>
+                    <div className="h3">+2.3%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="cta-container">
+              <Buy></Buy>
+              <Sell></Sell>
+              <div className="btn-main dark-bg--gradient shine-hover">
+                <FaMoneyBillTransfer className='money-svg-bg' />
+                <div>
+                  <FaMoneyBillTransfer className='money-svg' />
+                </div>
+                <div className="btn-main-container">
+                  <div className="h2">TRADE</div>
+                  <div>
+                    <FaArrowRight className="svg" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="snippet-container">
+              <div className="transactions-container dark-bg--gradient">
+                <LineChart />
+              </div>
+              <div className="transactions-container dark-bg--gradient">
+                <div className="transaction-heading">
+                  <div className="h2">Exchange rates</div>
+                  <button className="h4 tertiary-btn">view all</button>
+                </div>
+                <div className="transaction-heading">
+                  <div className="transaction-user-container">
+                    <div>
+                      <FaCoins className="svg" />
+                    </div>
+                    <div className="transaction-user-text-container">
+                      <div className="h3">STRC</div>
+                      <div className="h4">Star Chip</div>
+                    </div>
+                  </div>
+                  <div className="h3 green">R250.00  +2.5%</div>
+                </div>
+                <div className="line"></div>
+                <div className="transaction-heading">
+                  <div className="transaction-user-container">
+                    <div>
+                      <FaCoins className="svg" />
+                    </div>
+                    <div className="transaction-user-text-container">
+                      <div className="h3">STRC</div>
+                      <div className="h4">Star Chip</div>
+                    </div>
+                  </div>
+                  <div className="h3 green">R250.00  +2.5%</div>
+                </div>
+                <div className="line"></div>
+                <div className="transaction-heading">
+                  <div className="transaction-user-container">
+                    <div>
+                      <FaCoins className="svg" />
+                    </div>
+                    <div className="transaction-user-text-container">
+                      <div className="h3">STRC</div>
+                      <div className="h4">Star Chip</div>
+                    </div>
+                  </div>
+                  <div className="h3 green">R250.00  +2.5%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="body-container--right">
+            <div className="portfolio-heading-container">
+              <div className="portfolio-heading-sub-container">
+                <div className="portfolio-heading">Total Portfolio</div>
+                <button className="h4 tertiary-btn">view more</button>
+              </div>
+              <div className="line"></div>
+              <div className="portfolio-sub-heading">R {accountData ? accountData.balance.toFixed(2) : 'N/A'}</div>
+            </div>
+            {/* Coin */}
+            <div className="portfolio-card-container shine-hover">
+              <div className="coin-card dark-bg--gradient">
+                <div className='coin-card-shape shape1'></div>
+                <div className='coin-card-shape shape3'></div>
+                <div className='coin-card-shape shape2'></div>
+
+                <div className="h3">Star Coin</div>
+                <div className="coin-card--heading"> {accountData?.coinBalance ?? 0} <span className="coin-card--heading--small">STRP</span> </div>
+                <div className="h3 green">+2.3%</div>
+                <div className="blur-block"></div>
+              </div>
+            </div>
+            <div className="transactions-container dark-bg--gradient">
+              <div className="transactions-container-overlay"></div>
+              <div className="transaction-heading">
+                <div className="h2">Transactions</div>
+                <button className="h4 tertiary-btn">view all</button>
+              </div>
+              <div className="transaction-heading">
+                <div className="transaction-user-container">
+                  <div className="transaction-profile-image-container">
+                    <img src="https://images.unsplash.com/photo-1707928373566-6a8ac1308d9a?q=80&w=1546&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" className="profile-image" />
+                  </div>
+                  <div className="transaction-user-text-container">
+                    <div className="h3">Luca Breebaart</div>
+                    <div className="h4">31 july, 8pm</div>
+                  </div>
+                </div>
+                <div className="h3 green">Star Coin</div>
+              </div>
+              {/*  */}
+              <div className="line"></div>
+              {/*  */}
+              <div className="transaction-heading">
+                <div className="transaction-user-container">
+                  <div className="transaction-profile-image-container">
+                    <img src="https://images.unsplash.com/photo-1707928373566-6a8ac1308d9a?q=80&w=1546&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" className="profile-image" />
+                  </div>
+                  <div className="transaction-user-text-container">
+                    <div className="h3">Luca Breebaart</div>
+                    <div className="h4">31 july, 8pm</div>
+                  </div>
+                </div>
+                <div className="h3 green">Star Coin</div>
+              </div>
+              {/*  */}
+              <div className="line"></div>
+              {/*  */}
+              <div className="transaction-heading">
+                <div className="transaction-user-container">
+                  <div className="transaction-profile-image-container">
+                    <img src="https://images.unsplash.com/photo-1707928373566-6a8ac1308d9a?q=80&w=1546&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" className="profile-image" />
+                  </div>
+                  <div className="transaction-user-text-container">
+                    <div className="h3">Luca Breebaart</div>
+                    <div className="h4">31 july, 8pm</div>
+                  </div>
+                </div>
+                <div className="h3 green">Star Coin</div>
+              </div>
+              <NavBar></NavBar>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
